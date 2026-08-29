e4292b2f95e5cb3b5291a46ed03b4a43e6ecdcef
Jakob <siegerjakob@gmail.com>
2026-08-29 10:57:28 +0200

    added getters and setters for GhostNet and Person

--- src/main/java/de/jakobsieger/ghostnetfishing/model/GhostNet.java
+++ src/main/java/de/jakobsieger/ghostnetfishing/model/GhostNet.java
@@ -1,13 +1,71 @@
 package de.jakobsieger.ghostnetfishing.model;
 
 public class GhostNet {
-	
+
 	private int id;
 	private double latitude;
 	private double longitude;
 	private double size;
 	private GhostNetStatus status;
-	private Person reported_by;
-	
-	public GhostNet() {}
+	private Person reportedBy;
+	private Person salvageRegisteredBy;
+
+	public GhostNet() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getSize() {
+		return size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public GhostNetStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(GhostNetStatus status) {
+		this.status = status;
+	}
+
+	public Person getReportedBy() {
+		return reportedBy;
+	}
+
+	public void setReportedBy(Person reportedBy) {
+		this.reportedBy = reportedBy;
+	}
+
+	public Person getSalvageRegisteredBy() {
+		return salvageRegisteredBy;
+	}
+
+	public void setSalvageRegisteredBy(Person salvageRegisteredBy) {
+		this.salvageRegisteredBy = salvageRegisteredBy;
+	}
 }
--- src/main/java/de/jakobsieger/ghostnetfishing/model/Person.java
+++ src/main/java/de/jakobsieger/ghostnetfishing/model/Person.java
@@ -1,10 +1,35 @@
 package de.jakobsieger.ghostnetfishing.model;
 
 public class Person {
-	
+
 	private int id;
 	private String name;
 	private String phone;
-	
-	public Person() {}
+
+	public Person() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
